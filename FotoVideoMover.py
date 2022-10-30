e6131d7a5b37a14178ccf9dca355f5dfd1a0d60f
pythoniac <pythoniac@users.noreply.github.com>
2022-10-30 15:04:47 +0100

    some updates and testing

--- FotoVideoMover.py
+++ FotoVideoMover.py
@@ -24,91 +24,107 @@
 import os, sys, re, shutil
 from glob import glob
 import subprocess as sp
+from typing import Optional
 
 try:
     import exifread
-except Exception as e:
-    print('Import error: '+ str(e))
-    print('please do ''pip install exifread''')
-    input('<return>')
+except Exception as err:
+    print(f"Import error: {err}")
+    print("please do " "pip install exifread" "")
+    input("<return>")
     sys.exit(0)
 
 
 # GLOBALS
-folderList =[]
-FFPROBE_BIN = "ffprobe.exe" # on Windows
-extensions = ['jpg', 'mp4'] # what extensions to search for in the dropped folder
-fotos = ['tif', 'jpg', 'raw'] # list of foto file formats - can be extended for future use
-videos = ['avi', 'mp4', 'mov'] # same for video to let the program know what is video or foto
-CHECKMODE = False #if True: skips foto processing; switches to video and outputs results of ffprobe for debugging
+folderList = []
+FFPROBE_BIN = "ffprobe.exe"  # on Windows
+extensions = ["jpg", "mp4"]  # what extensions to search for in the dropped folder
+fotos = [
+    "tif",
+    "jpg",
+    "raw",
+]  # list of foto file formats - can be extended for future use
+videos = [
+    "avi",
+    "mp4",
+    "mov",
+]  # same for video to let the program know what is video or foto
+CHECKMODE = False  # if True: skips foto processing; switches to video and outputs results of ffprobe for debugging
 
 # FUNCTIONS
-def init():
-    if len(sys.argv)> 2:
-        input('Drop only one folder at a time!\nhit <enter>')
+def init() -> Optional[str]:
+    if len(sys.argv) > 2:
+        input("Drop only one folder at a time!\nhit <enter>")
         quit()
     elif len(sys.argv) == 1:
-        input('Drop one folder at this script!\nhit <enter>')
-        quit()   
-
-    path=(str(sys.argv[1]))
-    if not os.path.isdir(path):
-        input('Must be a folder not a file!\nhit <enter>')
+        input("Drop one folder at this script!\nhit <enter>")
         quit()
 
-    print('\nFireing up on...')
-    print(path)
-    return(path)
+    path_str = str(sys.argv[1])
+    if not os.path.isdir(path_str):
+        input("Must be a folder not a file!\nhit <enter>")
+        quit()
 
-def videoDate(fileName):
+    print("\nFireing up on...")
+    print(path_str)
+    return path_str
+
+
+def videoDate(fileName: str) -> Optional[str]:
     command = [FFPROBE_BIN, fileName]
     try:
         info = sp.getoutput(command)
     except sp.CalledProcessError as err:
-        raise RuntimeError("command '{}' returns error (code {}): {}".format(err.cmd, err.returncode, err.output))
+        raise RuntimeError(
+            "command '{}' returns error (code {}): {}".format(
+                err.cmd, err.returncode, err.output
+            )
+        )
 
     if CHECKMODE == True:
-        os.system('cls')  # for Windows
-        #os.system('clear')  # for Linux/OS X
-        print(fileName,'\n\n')
-        print (info,'\n')
-        m=re.search('creation_time.*?(\d{4}-\d{2}-\d{2})',info)
+        os.system("cls")  # for Windows
+        # os.system('clear')  # for Linux/OS X
+        print(fileName, "\n\n")
+        print(info, "\n")
+        m = re.search("creation_time.*?(\d{4}-\d{2}-\d{2})", info)
         if m:
-            print('match:\n' + m.group(1))
-        answer = input('\n<q> to quit or <enter> to continue: ')
-        if answer.lower() in ['q']:
+            print("match:\n" + m.group(1))
+        answer = input("\n<q> to quit or <enter> to continue: ")
+        if answer.lower() in ["q"]:
             quit()
         else:
-            return 0
+            return None
 
-    m=re.search('creation_time.*?(\d{4}-\d{2}-\d{2})',info)
+    m = re.search("creation_time.*?(\d{4}-\d{2}-\d{2})", info)
     if m:
         return str(m.group(1))
     else:
-        return 0
+        return None
 
 
-def fotoDate(fileName):
-    f = open(fileName, 'rb')
-    tags = exifread.process_file(f, stop_tag='EXIF DateTimeOriginal')
-    f.close
-    creationDateTime = 0
+def fotoDate(fileName: str) -> Optional[str]:
+    with open(fileName, "rb") as f_handle:
+        tags = exifread.process_file(f_handle, stop_tag="EXIF DateTimeOriginal")
+
+    creationDateTime = None
     try:
-        creationDateTime = (tags['EXIF DateTimeOriginal'])
-    except:
+        creationDateTime = tags["EXIF DateTimeOriginal"]
+    except KeyError:
         try:
-            f = open(fileName, 'rb')
-            tags = exifread.process_file(f)
-            f.close
-            creationDateTime = (tags['Image DateTime'])
-        except:
-            pass
+            with open(fileName, "rb") as f_handle:
+                tags = exifread.process_file(f_handle)
+            creationDateTime = tags["Image DateTime"]
+        except KeyError:
+            for k, v in tags:
+                print(f"{k}: {v}")
+            input()
+            
     if creationDateTime:
-        creationDateTime=str(creationDateTime)
+        creationDateTime = str(creationDateTime)
         year = creationDateTime[:4]
         month = creationDateTime[5:7]
         day = creationDateTime[8:10]
-        creationDateTime = str(year+'-'+month+'-'+day)
+        creationDateTime = str(year + "-" + month + "-" + day)
 
     return creationDateTime
 
@@ -122,49 +138,50 @@ def folders(date):
         os.makedirs(date)
         folderList.append(date)
 
+
 # MAIN
-path = init()
-os.chdir(path)
+if __name__ == "__main__":
+    path = init()
+    os.chdir(path)
 
-answer = input('\ncontinue (y = yes / c = checkmode / others abort): ')
+    answer = input("\ncontinue (y = yes / c = checkmode / others abort): ")
 
-if not answer.lower() in ['y', 'yes', '1', 'c']:
-    quit()
+    if not answer.lower() in ["y", "yes", "1", "c"]:
+        quit()
 
-if answer.lower() == 'c': # checkmode is ON...
-    CHECKMODE = True
-    print('\n\nyou have activated checkmode!')
-    print('skipping all fotos, no folder creation, no file moving')
-    print('now only outputting results of ffprobe video results')
-    input('hit <enter>! ')
+    if answer.lower() == "c":  # checkmode is ON...
+        CHECKMODE = True
+        print("\n\nyou have activated checkmode!")
+        print("skipping all fotos, no folder creation, no file moving")
+        print("now only outputting results of ffprobe video results")
+        input("hit <enter>! ")
 
-if CHECKMODE == False:
-    print('\nprocessing EXIF\n')
+    if CHECKMODE == False:
+        print("\nprocessing EXIF\n")
 
-for extension in extensions:
-    date = 0
-    counter = 0
-    extension = '*.'+extension
-    fileList = glob(extension)
-    filesTotal = len(fileList)
-    print('\n\n',filesTotal,'files for extension',extension)
-    for file in fileList:
-        counter += 1
-        progress = 100*counter/filesTotal
+    for extension in extensions:
+        date = 0
+        counter = 0
+        extension = "*." + extension
+        fileList = glob(extension)
+        filesTotal = len(fileList)
+        print("\n\n", filesTotal, "files for extension", extension)
+        for file in fileList:
+            counter += 1
+            progress = 100 * counter / filesTotal
 
-        if extension[2:] in fotos and CHECKMODE == False:
-            date = fotoDate(file)
-        elif extension[2:] in videos:
-            date = videoDate(file)
+            if extension[2:] in fotos and CHECKMODE == False:
+                date = fotoDate(file)
+            elif extension[2:] in videos:
+                date = videoDate(file)
 
-        if date and CHECKMODE == False:
-            folders(date)
-            shutil.move(file, date)
+            if date and CHECKMODE == False:
+                folders(date)
+                shutil.move(file, date)
 
-        if CHECKMODE == False:
-            sys.stdout.write("progress: %i%%   \r" % (progress))
-            sys.stdout.flush()
+            if CHECKMODE == False:
+                sys.stdout.write("progress: %i%%   \r" % (progress))
+                sys.stdout.flush()
 
-print('\n')
-input('finished - hit <enter>')
-
+    print("\n")
+    input("finished - hit <enter>")
